ff57a824113376570c2360306f50e0ce3f31eb19
Mori <laurent.ferric@gmail.com>
2025-06-30 11:19:57 +0200

    Initial project commit : README & pseudocode algorithm

---
+++ src/main/java/fr/LaurentFE/forestFire/ForestFire.java
@@ -0,0 +1,24 @@
+package fr.LaurentFE.forestFire;
+
+public class ForestFire {
+    public static void main(String[] args) {
+        /*
+         * Envisioned Algorithm
+         * Load config file (h, l, p, (x,y) of ignited trees, maybe GUI settings too)
+         * Create array[h][l] of Tree {state, pos, neighbours=new set<Tree>, void addNeighbour(Tree), bool ignite(p)}
+         * ignite(p) checks for state, plays probability check, then modifies state and answers true if successful.
+         * Compose graph of Tree by adding neighbours in accordance to graph
+         * Ignite initial tree(s) and add them to a set
+         * While there is a tree in set
+         *   For each tree
+         *     For each neighbour
+         *       Try to ignite neighbour
+         *       If ignite successful
+         *         Add ignited tree to a new set
+         *   Replace set with new set
+         *   Display state of the forest using array of Tree
+         *   Wait for user action
+         * No tree is burning, end of simulation.
+         */
+    }
+}
